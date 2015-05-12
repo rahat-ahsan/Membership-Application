84e33c7ac3ea09f62de8bb2e515fe842377edf65
Rahat Ahsan Chowdhury <rahat27@gmail.com>
2015-05-12 20:46:28 +0200

    User Panel Updated

---
+++ Memberships/Memberships/ViewModel/UserModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Memberships.ViewModel
+{
+    public class UserModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        [StringLength(30, ErrorMessage = "The {0} must be at least {2} characters long.", MinimumLength = 2)]
+        public string Name { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "The {0} must be at least {2} characters long. Have 1 non letter, 1 digit, 1 uppercase ('A'-'Z').", MinimumLength = 6)]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Required]
+        public bool AcceptUserAgrrement { get; set; }
+    }
+}
